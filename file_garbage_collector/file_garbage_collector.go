3f9844225803a8cf41c0431704dacbe215010177
Thomas Barton <thomasryanbarton@gmail.com>
2018-07-05 16:34:17 -0700

    Add garbage collector tickID delete empty directories (#9)

--- file_garbage_collector/file_garbage_collector.go
+++ file_garbage_collector/file_garbage_collector.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/image-server/image-server/core"
 	"path/filepath"
+	"io"
 )
 
 func Start(sc *core.ServerConfiguration) {
@@ -26,23 +27,55 @@ func Start(sc *core.ServerConfiguration) {
 					return nil
 				})
 				for range sc.CleanUpTicker.C {
+					tickTime := time.Now()
+					log.Printf("Starting [tickID: %v]\n", tickTime)
+					stepNum := 0
 					filepath.Walk(absolutePath, func(path string, info os.FileInfo, err error) error {
-						if !info.IsDir() {
-							age := time.Now().Sub(info.ModTime())
+						stepNum = stepNum + 1
+						if err != nil {
+							return err
+						}
+						if info.IsDir() {
+							empty, err := IsDirectoryEmpty(path)
+							if err != nil {
+								return err
+							}
+							if empty {
+								age := tickTime.Sub(info.ModTime())
+								log.Printf("[tickID: %v] Deleting directory [%s] size [%d] modTime [%s] age [%s]\n", tickTime, path, info.Size(), info.ModTime(), age)
+								os.Remove(path)
+							}
+						} else {
+							age := tickTime.Sub(info.ModTime())
 							if age > sc.MaxFileAge {
-								log.Printf("Deleting file [%s] size [%d] modTime [%s] age [%s]\n", path, info.Size(), info.ModTime(), age)
+								log.Printf("[tickID: %v] Deleting file [%s] size [%d] modTime [%s] age [%s]\n", tickTime, path, info.Size(), info.ModTime(), age)
 								var err = os.Remove(path)
 								if err != nil {
-									log.Printf("Error deleting file [%s]\n", path)
+									log.Printf("[tickID: %v] Error deleting file [%s]\n", tickTime, path)
 								}
 							}
 						}
 						return nil
 					})
+					log.Printf("Finished [tickID: %v] in [%v] steps\n", tickTime, stepNum)
 				}
 			} else {
 				log.Printf("Error Starting File Cleaner - Invalid walk path [%s]", absolutePath)
 			}
 		}
 	}()
+}
+
+func IsDirectoryEmpty(path string) (bool, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return false, err
+	}
+	defer f.Close()
+
+	_, err = f.Readdirnames(1) // Or f.Readdir(1)
+	if err == io.EOF {
+		return true, nil
+	}
+	return false, err // Either not empty or error, suits both cases
 }
