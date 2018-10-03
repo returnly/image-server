f7593ca416b6eac218fc16ab7dc0ca7e42b98dc0
Thomas Barton <thomasryanbarton@gmail.com>
2018-10-03 14:11:11 -0700

    Remove cobra config

--- cmd/root.go
+++ cmd/root.go
@@ -13,9 +13,7 @@ import (
 	"github.com/image-server/image-server/logger/statsd"
 	"github.com/image-server/image-server/paths"
 	"github.com/image-server/image-server/uploader"
-	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 // configT collects all the global state of the logging setup.
@@ -60,7 +58,6 @@ type configT struct {
 	version bool
 }
 
-var cfgFile string
 var config configT
 
 // RootCmd represents the base command when called without any subcommands
@@ -75,41 +72,6 @@ func Execute() {
 	if err := RootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
-	}
-}
-
-func init() {
-	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cobra-example.yaml)")
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".cobra-example" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".cobra-example")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
 	}
 }
 
