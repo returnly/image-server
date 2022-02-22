0896339d80c4097a3a144d70922c9ad381725ee0
Alex Martín <alex.martin@returnly.com>
2022-02-22 16:28:54 +0100

    Remove unnecessary var and add comments

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@
 set -euo pipefail
 
 SUPPORTED_ARCHS="amd64 arm64"
-REGISTRY_URI="${REGISTRY_URI:-887832589804.dkr.ecr.us-west-1.amazonaws.com/image-server}"
 APP_REPO_TAG=$REGISTRY_URI:$DOCKER_TAG
 
 # build individual architectures
@@ -17,14 +16,15 @@ for ARCH in $SUPPORTED_ARCHS; do
   ALL_ARCH_TAGS="$ALL_ARCH_TAGS $TAG"
 done
 
-# create multi-arch manifest
+# Create multi-arch manifest
 echo "*** Creating manifest and pushing $APP_REPO_TAG ***"
 docker manifest create $APP_REPO_TAG $ALL_ARCH_TAGS
 for ARCH in $SUPPORTED_ARCHS; do
   docker manifest annotate --arch $ARCH $APP_REPO_TAG $APP_REPO_TAG-$ARCH
 done
-
 docker manifest push $APP_REPO_TAG
+
+# Annotate build
 if [ -x "$(command -v buildkite-agent)" ]; then
-  buildkite-agent annotate "Image tag: $BGH_RETURNLY_DOCKER_TAG" --style 'info' --context 'build-info'
+  buildkite-agent annotate "Image: $APP_REPO_TAG" --style 'info' --context 'build-info'
 fi
